ae6c8f7b2bd20eb47757ce2046a72296093be3d4
ArmaanS1231 <armaanshah1231@gmail.com>
2024-07-27 21:19:41 -0400

    Update main.py
    Implemented python pirate translator library as a fucntion to use in html

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import arrr # type: ignore
+from pyscript import document # type: ignore
+
+def translate_english(event):
+  input_text = document.querySelector("#english")
+  english = input_text.value
+  output_div = document.querySelector("#output")
+  output_div.innerText = arrr.translate(english)
