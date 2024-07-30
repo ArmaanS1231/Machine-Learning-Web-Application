350f574ad1340e78d37bb46ce8e86600eddc8360
ArmaanS1231 <armaanshah1231@gmail.com>
2024-07-29 20:02:44 -0400

    Update main.py
    decided py-script wouldn't be a good fit, so decided to try making a model in flask. Today I stopped experimenting with py-script and decided to make a sample model that I can use with flask. This is a model that I created that predicts if a person is likely to have heart disease

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-import arrr # type: ignore
-from pyscript import document # type: ignore
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+import pickle
 
-def translate_english(event):
-  input_text = document.querySelector("#english")
-  english = input_text.value
-  output_div = document.querySelector("#output")
-  output_div.innerText = arrr.translate(english)
+
+df = pd.read_csv('heart.data.csv')
+
+df = df.drop("Unnamed: 0", axis=1)
+
+sns.lmplot(x='biking', y='heart.disease', data=df)
+sns.lmplot(x='smoking', y='heart.disease', data=df)
+
+x_df = df.drop('heart.disease', axis=1)
+y_df= df['heart.disease']
+
+X_train, X_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.3, random_state = 42)
+
+
+model=linear_model.LinearRegression()
+model.fit(X_train, y_train)
+predicition_test = model.predict(X_test)
+
+print(model.score(X_train,y_train))
+print(np.mean(predicition_test-y_test)**2)
+
+
+pickle.dump(model, open('model.pk1','wb'))
+model = pickle.load(open('model.pkl','rb'))
+
+print(model.predict([70.1, 26.3]))
+
